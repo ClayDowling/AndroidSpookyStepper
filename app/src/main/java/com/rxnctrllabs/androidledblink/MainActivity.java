3086f3c16e818c75826f274808450dbb5292c294
tclchiam <tchupp@pillartechnology.com>
2016-12-27 11:06:12 -0500

    Got an LED blinking on GPIO port 21!

--- app/src/main/java/com/rxnctrllabs/androidledblink/MainActivity.java
+++ app/src/main/java/com/rxnctrllabs/androidledblink/MainActivity.java
@@ -3,28 +3,70 @@ package com.rxnctrllabs.androidledblink;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
+import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManagerService;
 
+import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private Gpio gpio = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Log.d(TAG, "onCreate");
+    @Override
+    protected void onPostCreate(final Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        listAvailableGpio();
+        connectToGpioPort("BCM21");
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (null != gpio)
+            try {
+                gpio.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        super.onDestroy();
+    }
+
+    private void listAvailableGpio() {
+        TextView availableGpioText = (TextView) findViewById(R.id.availableGpioText);
 
         PeripheralManagerService manager = new PeripheralManagerService();
         List<String> portList = manager.getGpioList();
-        if (portList.isEmpty()) {
-            Log.i(TAG, "No GPIO port available on this device.");
-        } else {
-            Log.i(TAG, "List of available ports: " + portList);
+
+        availableGpioText.setText(portList.toString());
+    }
+
+    private void connectToGpioPort(String gpioName) {
+        try {
+            PeripheralManagerService manager = new PeripheralManagerService();
+            gpio = manager.openGpio(gpioName);
+
+            gpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
+            gpio.setActiveType(Gpio.ACTIVE_LOW);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Log.w(TAG, "Thread sleep exception", e);
+            }
+
+            gpio.setValue(true);
+        } catch (IOException e) {
+            Log.w(TAG, "Unable to access GPIO", e);
         }
     }
 }
